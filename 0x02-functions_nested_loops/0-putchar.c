c3a788f349eff4bacc25616264623cad52d7e9b7
Rido0809ja <mifun45@gmail.com>
2023-09-13 14:03:19 -0700

    _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+/*
+*main: print _putchar
+*
+*prints _putchar
+*
+*return 0 Always success
+*
+*/
+
+int main (void)
+{
+    printf("_putchar\n");
+    
+    return (0);
+    
+}
+
