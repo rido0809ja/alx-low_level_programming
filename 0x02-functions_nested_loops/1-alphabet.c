18a69341ed7627ba6dc517c7b6ba27b86cf59fce
Rido0809ja <mifun45@gmail.com>
2023-09-23 16:39:18 -0700

    Alphabet

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -2,7 +2,7 @@
 
 /**
  * print_alphabet - Entry point
- * Return:0
+ * 
  */
 
 void print_alphabet(void)
@@ -12,5 +12,5 @@ for (c = 'a' ; c <= 'z'; c++)
 {
 _putchar(c);
 }
-_putchar("\n");
+_putchar('\n');
 }
